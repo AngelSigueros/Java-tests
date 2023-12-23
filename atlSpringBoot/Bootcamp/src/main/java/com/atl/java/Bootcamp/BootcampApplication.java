2c49f8b124c9a7766bfdd935e4d62c87f9b19359
AngelSigueros <pickassblog@gmail.com>
2023-12-24 00:36:39 +0100

    BootCamp primer Spring Boot

---
+++ atlSpringBoot/Bootcamp/src/main/java/com/atl/java/Bootcamp/BootcampApplication.java
@@ -0,0 +1,13 @@
+package com.atl.java.Bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampApplication.class, args);
+	}
+
+}
---
+++ atlSpringBoot/Bootcamp/src/main/java/com/atl/java/Bootcamp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.atl.java.Bootcamp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootcampApplication.class);
+	}
+
+}
