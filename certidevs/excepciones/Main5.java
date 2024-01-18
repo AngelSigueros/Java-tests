cc6c1d2af698f1b19794c67ac178903ae854d073
AngelSigueros <pickassblog@gmail.com>
2024-01-18 19:20:04 +0100

    Update Main5.java

--- certidevs/excepciones/Main5.java
+++ certidevs/excepciones/Main5.java
@@ -1,9 +1,20 @@
 package com.certidevs.excepciones;
 
+import java.io.IOException;
+
 public class Main5 {
 
     public static void main(String[] args) {
 
+        try {
+            realizarAccion();
+        } catch (IOException e) {
+            System.out.println("No se ha podido leer el archivo");
+        }
+    }
 
+    public static void realizarAccion() throws IOException {
+
+        throw new IOException();
     }
 }
